23577fa43e9b1ba3dd193d8ba29571f03136f236
jojolin <ljzjojop@126.com>
2020-08-04 10:06:36 +0800

    chrome+plugins+scripts将运行时变成服务

---
+++ chrome-plugins-and-scripts/README.md
@@ -0,0 +1,6 @@
+# 将chrome运行时变成服务
+- 利用`chrome plugin + serve proxy + mock js`
+- 参考代码: `inject-and-serve.js`
+
+## 应用场景
+- 获取chrome脚本运行时环境，执行自定义数据处理逻辑
---
+++ chrome-plugins-and-scripts/inject-and-serve.js
@@ -0,0 +1,32 @@
+var pingpong = (function () {
+    'use strict'
+    return function _pingpong() {
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open("GET", "http://your-own-serv-url"); ;
+
+        xhr.onload = function () {
+            // acquire the data
+            var data = xhr.response;
+            console.log(data);
+
+            /*
+            TODO: add your own inject code here.
+            handle your datalogic.
+             */
+            var handled_data = dataLogic(data);
+
+            var xhr2 = new XMLHttpRequest();
+            xhr2.open("POST", "http://your-own-serv-url");
+            // send back handled data
+            xhr2.send(handled_data);
+
+            // keep going!
+            _pingpong();
+
+        };
+        xhr.send();
+    }
+})();
+pingpong();
